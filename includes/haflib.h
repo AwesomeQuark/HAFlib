dce5772735bfa11ffc70b3d2af77f3e323fdf551
corentin <corentin.noel@aliceadsl.fr>
2019-04-08 15:02:36 +0200

    new organisation header

--- includes/haflib.h
+++ includes/haflib.h
@@ -6,7 +6,7 @@
 /*   By: conoel <conoel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 18:03:06 by conoel            #+#    #+#             */
-/*   Updated: 2019/03/19 16:54:16 by conoel           ###   ########.fr       */
+/*   Updated: 2019/04/08 15:02:06 by conoel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,18 @@
 # include <stdarg.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include <string.h>
-# include <limits.h>
-# include <errno.h>
 # include <sys/types.h>
 # include <sys/uio.h>
+
+# include "garbage.h"
+# include "ft_printf.h"
+# include "quit.h"
+# include "ft_is.h"
 
 # define BUFF_SIZE 16
 # define ITER_MAX (INT_MAX / BUFF_SIZE)
 
-int					ft_printf(const char *str, ...);
 
-void				exit_free(char *msg, int format, ...);
-void				exit_(char *msg);
-size_t				return_free(char *msg, int format, ...);
-size_t				return_(char *msg);
 void				free_tab(char **tab);
 
 char				*ft_strjoin(char const *s1, char const *s2);
@@ -49,18 +46,5 @@ size_t				ft_strlen(const char *s);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 void				*ft_memchr(const void *s, int c, size_t n);
-
-int					ft_isalnum(int c);
-int					ft_isalpha(int c);
-int					ft_isascii(int c);
-int					ft_isdigit(int c);
-int					ft_isprint(int c);
-int					ft_isspace(int c);
-int					ft_isblank(int c);
-int					ft_iscntrl(int c);
-int					ft_isgraph(int c);
-int					ft_islower(int c);
-int					ft_isupper(int c);
-int					ft_isalphanum_base(int c, int base, int maj);
 
 #endif
